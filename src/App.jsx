755378ae03c9b7d72d681ea6547e5424a6d6384c
himavarshini28 <himavarshinimahapatruni@gmail.com>
2024-09-19 23:15:30 +0530

    Pages and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import Profile from './pages/Profile'
+import About from './pages/About'
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div className='text-red-500'>
-      APP
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+      <Route path='/sign-in' element={<SignIn/>}/>
+      <Route path='/sign-up' element={<SignUp/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
